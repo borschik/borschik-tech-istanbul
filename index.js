b6c1dcc42e411eff03211fe05b7da0e370259c40
Vladimir Alaev <scff@yandex-team.ru>
2014-08-06 11:53:00 +0400

    Add instrumentPaths option to make possible specify which paths should be instrumented

--- index.js
+++ index.js
@@ -8,6 +8,23 @@ module.exports = function(borschik) {
     var base = borschik.getTech('js'),
         File = base.File.inherit({
             read: function() {
+                var ip = this.tech.opts.techOptions.instrumentPaths;
+                // Validate every include path if instrumentPaths option is specified
+                if (ip) {
+                    var instrument = false;
+                    for(var p = 0; p < ip.length; p++) {
+                        if (this.path.indexOf(ip[p]) === 0) {
+                            instrument = true;
+                            break;
+                        }
+                    }
+
+                    // include path doesn't match any path from instrumentPaths
+                    // -> don't instrument it and process as normal js.
+                    if (!instrument) {
+                        return this.__base();
+                    }
+                }
                 var content = fs.readFileSync(this.processPath(this.path), 'utf8'),
                     instrumented = instrumenter.instrumentSync(content, path.relative(cwd, this.path));
                 this.content = this.parse(instrumented);
